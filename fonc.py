a7eaef4be439a49b9e93fa883327e33cdcd0dc69
kiruuuto <kirouto442@gmail.com>
2025-10-02 21:29:18 +0200

    jai ajt les fonctions add demande et add projet

--- fonc.py
+++ fonc.py
@@ -6,9 +6,7 @@ def connect_db(db_name):
     print(f"Connecté à : {db_name}")
     return conn
 
-
-
-
+#Fonctions pour manipuler la table Clients
 
 def add_user(conn, username, email):
     """Ajoute un utilisateur à la base de données."""
@@ -26,24 +24,33 @@ def add_user(conn, username, email):
     conn.commit()
     print(f"User {username} added successfully.")
 
-
-
 def delete_user(conn, user_id):
     """Supprime un utilisateur de la base de données."""
     try:
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM Clients WHERE id = ?", (user_id,))
+        cursor.execute("DELETE FROM Clients WHERE username = ?", (user_id,))
         conn.commit()
         print(f"User {user_id} supprimé de Clients")
     except sqlite3.Error as e:
         print(f"Error deleting user: {e}")
 
+# Fonctions pour manipuler la table Demandes
+def add_demande(conn, user_id, request_text):
+    """Ajoute une demande à la base de données."""
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO Demandes (user_id, request_text) VALUES (?, ?)", (user_id, request_text))
+    conn.commit()
+    print(f"Request added for user {user_id}")
 
-
-
+# Fonctions pour manipuler la table Projets
+def add_projet(conn, idClient, idProjet, intitule, deadline, commentaire):
+    """Ajoute un projet à la base de données."""
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO Projets (idClient, idProjet, intitule, deadline, description) VALUES (?, ?, ?, ?, ?)", (idClient, idProjet, intitule, deadline, commentaire))
+    conn.commit()
+    print(f"Project {intitule} added successfully.")
 
 conn = connect_db("testprojet")
-if conn:
-    add_user(conn, "testuser", "testuser@gmqil.com2")
-    conn.close()
 
+with conn:
+    add_projet(conn, 1, 1, "Projet Alpha", "2024-12-31", "Premier projet important")
